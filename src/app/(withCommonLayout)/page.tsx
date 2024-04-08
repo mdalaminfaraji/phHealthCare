29c8b7f0c5209e636f0faf9512327964ed5e91f8
alaminfaraji <alaminice1617@gmail.com>
2024-04-08 23:47:55 +0600

    landing Page

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,11 +1,18 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { Button } from "@mui/material";
+import HeroSection from "@/components/UI/Homepage/HeroSection/HeroSection";
+import Specialist from "@/components/UI/Homepage/Specialist/Specialist";
+import TopRatedDoctors from "@/components/UI/Homepage/TopRatedDoctors/TopRatedDoctors";
+import WhyUs from "@/components/UI/Homepage/WhyChooseUs/WhyUs";
 
 export default function Home() {
   return (
     <div>
-      Home page<Button>Contained</Button>
+      <HeroSection />
+      <Specialist />
+      <TopRatedDoctors />
+      <WhyUs />
     </div>
   );
 }
